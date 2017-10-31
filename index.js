0bdfd9ced7c467590a69bf10acdcc35b44982e8e
Taha PAKSU <tpaksu@gmail.com>
2017-10-31 17:01:23 -0300

    continuing development.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var babylon = require("babylon");
-var fs = require('fs');
+var fs = require('fs-extra');
 var util = require('util');
 var path = require('path');
 var _ = require('lodash');
 
 var codeReporter = function(baseReporterDecorator, config, logger, helper, formatError) {
     var failedSpecList = [];
+
+    this.onRunStart = function(browsersCollection){
+        console.log("hello world");
+    };
+
     this.onSpecComplete = function(browser, result) {
         if(result.success === false){
             failedSpecList.push([browser, result]);
@@ -18,13 +23,14 @@ var codeReporter = function(baseReporterDecorator, config, logger, helper, forma
                 var args = this.parseResult(failedSpecList[i][0], failedSpecList[i][1]);
                 var outputFilename = path.format({root: config.basePath, dir: path.normalize(config.codeReporter.outputPath), base: args.id + ".html" });
                 var content = this.createContent(config, args);
-                this.createFile(outputFilename, args);
+                this.createFile(outputFilename, content);
             }
         }
     };
 
     this.parseResult = function(browser, result){
         var args = {};
+        args.id = result.id;
         args.source = result.log[0].split("\n")[1];
         args.filename = args.source.split("?")[0].split("base")[1].substr(1);
         args.query = args.source.split("?")[1];
@@ -39,17 +45,114 @@ var codeReporter = function(baseReporterDecorator, config, logger, helper, forma
     };
 
     this.createContent = function(config, args){
-
-        var content = fs.readFileSync(args.filename, 'utf8');
-        console.log(args.filename, args.queryArgs.line, content.split('\n')[args.queryArgs.line - 1]);
-        //fs.writeFileSync("taha.txt", util.inspect(parsed) , 'utf-8');
+        var content = fs.readFileSync(args.filename, 'utf8').split('\n');
+        var bounds = this.findBounds(content);
+        var testcode = this.createTest(content, bounds, args.queryArgs.line);
+        console.log(testcode);
         return content;
-        //babylon.parse(content);
     };
 
-    this.createFile = function(filename, args){
+    this.createTest = function(content, bounds, errorLine) {
+        var applied = {
+            groups: [],
+            cases: [],
+            deconst: []
+        };
+
+        bounds.groups.forEach(function(bound) {
+            if(errorLine < bound[1] && errorLine > bound[0])
+                applied.groups.push(bound);
+        });
+
+        bounds.cases.forEach(function(bound){
+            if(errorLine < bound[1] && errorLine > bound[0])
+                applied.cases.push(bound);
+        });
+
+        bounds.deconst.forEach(function(bound){
+            applied.groups.forEach(function(group){
+                if(bound[0] > group[0] && bound[1] < group[1])
+                    applied.deconst.push(bound);
+            });
+        });
+
+        var lines = [];
+
+        applied.groups.forEach(function(group){
+            lines.push(group[0]);
+            lines.push(group[1]);
+        });
+
+        applied.deconst.forEach(function(deconst){
+            for(var d=deconst[0]; d<=deconst[1]; d++) lines.push(d);
+        });
+
+        applied.cases.forEach(function(scase){
+            for(var c=scase[0]; c<=scase[1]; c++) lines.push(c);
+        });
+
+            lines.sort()
+            lines = _.uniq(lines);
+
+        var result= [];
+        lines.forEach(function(line){
+            result.push(content[line]);
+        });
+
+        return result.join('\n');
 
     };
+
+    this.createFile = function(filename, content){
+        fs.createFileSync(filename);
+        fs.writeFileSync(filename, content, { encoding: 'utf8', flag: 'w'});
+    };
+
+    this.findBounds = function(content){
+        var bounds = { groups: [], cases: [], deconst: [] };
+        for(var i = 0; i < content.length; i++){
+            var regex1 = /describe\s*\(\s*(?:\"|')[^\"']+(?:\"|')\s*,\s*function\s*\(/i;
+            var regex2 = /it\s*\(\s*(?:\"|')[^\"']+(?:\"|')\s*,\s*function\s*\(/i;
+            var regex3 = /(?:beforeEach|afterEach)\s*\(\s*function\s*\(/i;
+            if((regex1.exec(content[i]) || []).length > 0){
+                bounds.groups.push([i,this.findEndLine(content, i)]);
+            }
+            else if((regex2.exec(content[i]) || []).length > 0){
+                bounds.cases.push([i,this.findEndLine(content, i)]);
+            }
+            else if((regex3.exec(content[i]) || []).length > 0){
+                bounds.deconst.push([i,this.findEndLine(content, i)]);
+            }
+        }
+        return bounds;
+    };
+
+    this.findEndLine = function(content, startLine){
+        var match = /\bfunction\s*\([^)]*\)\s*({)/i.exec(content[startLine]);
+        var pos = match[0].length + match.index - 1;
+        var $stack = [];
+        while(startLine < content.length){
+            while(pos < content[startLine].length){
+                var read = content[startLine][pos];
+                if(read == "{"){
+                    $stack.push("{");
+                }else if(read == "}"){
+                    var val = $stack.pop();
+                    if(val == "{"){
+                        if($stack.length == 0){
+                            return startLine;
+                        }
+                    }else{
+                        return -1;
+                    }
+                }
+                pos++;
+            }
+            startLine++;
+            pos = 0;
+        }
+        return -1;
+    }
 
 };
 
